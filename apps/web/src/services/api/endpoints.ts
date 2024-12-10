1077dc6c11922f47b835f66d5c796bcbcab86465
Pavel Glazunov <p6282813@yandex.ru>
2024-12-10 15:27:46 +0300

    [fix] url

--- apps/web/src/services/api/endpoints.ts
+++ apps/web/src/services/api/endpoints.ts
@@ -1,6 +1,6 @@
 const API_ENDPOINTS = {
   CHECK_USERNAME: "/auth/username",
-  AUTH_TELEGRAM_MINI_APP: "/auth/provider/TELEGRAM_MINI_APP",
+  AUTH_TELEGRAM_MINI_APP: "/auth/login",
   USER: "/users",
   USER_DEFAULT: "/user",
   STATS: "/stats/",
