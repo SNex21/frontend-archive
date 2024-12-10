6c13cca6687550a3eb0d7cfdb8f715ba7a0af746
Producer <99910126+SNex21@users.noreply.github.com>
2024-12-10 08:28:23 +0300

    Update endpoints.ts

--- apps/web/src/services/api/endpoints.ts
+++ apps/web/src/services/api/endpoints.ts
@@ -2,6 +2,7 @@ const API_ENDPOINTS = {
   CHECK_USERNAME: "/auth/username",
   AUTH_TELEGRAM_MINI_APP: "/auth/provider/TELEGRAM_MINI_APP",
   USER: "/users",
+  USER_DEFAULT: "/user",
   STATS: "/stats/",
   SESSION: "/tasks/session/",
   GET_TOPICS: "/tasks/topics/",
