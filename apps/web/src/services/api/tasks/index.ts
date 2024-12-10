af980b0e0ff0dcbbd28e6a077ba6fe1cb65554be
Producer <99910126+SNex21@users.noreply.github.com>
2024-12-10 22:24:42 +0300

    dfasfafda

--- apps/web/src/services/api/tasks/index.ts
+++ apps/web/src/services/api/tasks/index.ts
@@ -1,7 +1,7 @@
 import { GetTasksTopicsRes, GetTasksReq, GetTasksRes, CompleteSessionReq } from "@/services/api/tasks/types.ts";
 import { apiClient } from "@/services/api/client.ts";
 import { API_ENDPOINTS } from "@/services/api/endpoints.ts";
-import { Session, Challenge, Choice, DisplayToken} from "@/models/Session.ts";
+import { Session, Challenge, ChallengeType, Choice, DisplayToken} from "@/models/Session.ts";
 
 
 
@@ -26,42 +26,39 @@ export const getTasks = async ({ token, ...params }: GetTasksReq): Promise<GetTa
 };
 
 const mapSession = (apiData: any): Session => {
-  const session = {} as Session
+  
+  return {
+    id: apiData.id,
+    isHard: apiData.isHard,
+    isWorkOnMistakes: apiData.isWorkOnMistakes,
 
-  session.id = apiData.id
-  session.isHard = apiData.is_hard
-  session.isWorkOnMistakes = apiData.is_work_on_mistakes
-  session.topic = apiData.topic
-  session.amount = apiData.amount
-  session.challenges = apiData.challenges.map(mapChallenge)
-  return session;
+    topic: apiData.topic,
+    amount: apiData.amount,
+    challenges: apiData.challenges.map(mapChallenge),
+  };
 };
 
 const mapChallenge = (apiChallenge: any): Challenge => {
-  const challenge = {} as Challenge
-
-  challenge.id = apiChallenge.id
-  challenge.type = apiChallenge.slug
-  challenge.prompt = apiChallenge.prompt
-  challenge.displayTokens = apiChallenge.tokens?.map(mapDisplayToken)
-  challenge.choices = apiChallenge.answers?.map(mapChoice)
-  challenge.isHard = apiChallenge.is_hard
-  challenge.isWorkOnMistakes = apiChallenge.is_work_on_mistakes
-  challenge.explanation = apiChallenge.comment
-  return challenge;
+  return {
+    id: String(apiChallenge.id),
+    type: apiChallenge.slug,
+    prompt: apiChallenge.prompt,
+    displayTokens: apiChallenge.tokens?.map(mapDisplayToken),
+    choices: apiChallenge.answers?.map(mapChoice),
+    isHard: apiChallenge.is_hard,
+    isWorkOnMistakes: apiChallenge.is_work_on_mistakes,
+    explanation: apiChallenge.comment,
+  };
 };
 
-// const mapChallengeType = (type: string): ChallengeType => {
-//   const challenge_type = {} as ChallengeType
-
-//   challenge_type.
-//   // switch (type) {
-//   //   case "string":
-//   //     return "gapFill";
-//   //   default:
-//   //     throw new Error(`Unknown challenge type: ${type}`);
-//   // }
-// };
+const mapChallengeType = (type: string): ChallengeType => {
+  switch (type) {
+    case "string":
+      return "gapFill";
+    default:
+      throw new Error(`Unknown challenge type: ${type}`);
+  }
+};
 
 const mapDisplayToken = (apiToken: any): DisplayToken => {
   return {
