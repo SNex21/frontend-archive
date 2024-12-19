a086bfc94df118d152b43abc32582333cf5dc8d5
Jacob <stupnikovd51@gmail.com>
2024-12-19 13:14:48 +0300

    poprobocat

--- apps/web/src/pages/(authenticated)/(with-tabbar)/theory/page.tsx
+++ apps/web/src/pages/(authenticated)/(with-tabbar)/theory/page.tsx
@@ -5,10 +5,23 @@ import styles from "./Theory.module.scss";
 
 export default function TheoryPage() {
   return (
-    <div className={pageStyles.main}>
-      <div className={styles.overlayLine}></div>
-      <div className={styles.overlayLine}></div>
-      <TheorySection />
-    </div>
+    <><div
+      className={styles.container}
+      style={{ backgroundImage: 'frontend-archive/apps/web/src/pages/(authenticated)/(with-tabbar)/theory/Gradient.png' }}
+    >
+      <img
+        src="frontend-archive/apps/web/src/pages/(authenticated)/(with-tabbar)/theory/Vector.png" 
+        alt="Логотип"
+        className={styles.logo} />
+      <p className={styles.text}>
+        Раздел <span className={styles.highlight}>Теория</span> находится в активной
+        разработке команды Учибота. Следи за обновлениями в Telegram канале.
+      </p>
+      <a href="LINK_TO_TELEGRAM_CHANNEL" target="_blank" rel="noopener noreferrer">
+        <button className={styles.button}>Перейти в канал</button>
+      </a>
+    </div><div className={pageStyles.main}>
+        <TheorySection />
+      </div></>
   );
 }
