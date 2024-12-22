b3e87b7286d8bc4c9379c0cf4312f1d320e504ca
Producer <99910126+SNex21@users.noreply.github.com>
2024-12-23 02:42:33 +0300

    add api getting subs

--- apps/web/src/services/api/subscriptions/index.ts
+++ apps/web/src/services/api/subscriptions/index.ts
@@ -1,10 +1,11 @@
 import { Subscription } from "@/models";
 import { apiClient } from "../client";
 import { API_ENDPOINTS } from "../endpoints";
+import { GetSubInfoProps } from "./types";
 
 
 export const getSubscriptionInfo = async ({ token }: GetSubInfoProps): Promise<Subscription> => {
-  const { data } = await apiClient.get<User>(`${API_ENDPOINTS.GET_SUBSCRIPTION_INFO}`, {
+  const { data } = await apiClient.get<Subscription>(`${API_ENDPOINTS.GET_SUBSCRIPTION_INFO}`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
