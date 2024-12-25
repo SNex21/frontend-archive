9c6d9c152878e9a214a5ef9ecf3f13c01d670d50
Producer <99910126+SNex21@users.noreply.github.com>
2024-12-26 01:49:39 +0300

    create sub page

--- apps/web/src/pages/(authenticated)/subscription/page.tsx
+++ apps/web/src/pages/(authenticated)/subscription/page.tsx
@@ -1,4 +1,3 @@
-import { useNavigate } from "react-router-dom";
 import { FC } from "react";
 import { Haptic } from "@/lib/twa/components/Haptic";
 import { Link } from "react-router-dom";
@@ -6,30 +5,46 @@ import styles from "./Account.module.scss";
 import cn from "classnames";
 
 import { useUser } from "@/providers/AuthProvider/AuthProvider";
+import { useCloudStorage } from "@/lib/twa/hooks";
+import { ACCESS_TOKEN_NAME } from "@/services/auth/storage.ts";
+import { useQuery } from "@tanstack/react-query";
+import { getSubscriptionInfo } from "@/services/api/subscriptions";
 
 export default function SubscriptionPage() {
-  const navigate = useNavigate();
   const user = useUser();
-
-  return (
-    <section className={cn("wrapper", styles.section)}>
-      <h2 className={styles.section__heading}>Подписка</h2>
-      {!user.subscription ? (
-        <SubscriptionCard
-        title="У тебя пока нет подписки"
-        description="Открывай доступ ко всем заданиям с подпиской!"
-        href={"/"}
-      />
-      ) : (
-        <SubscriptionCard
-        title={`Подписка ${subscription.plan.title}`}
-        description={`Действует до ${formatDate(subscription.end_date)}`}
-        href={"/"}
-      />
-      )}
-    </section>
-    )
+  const cloudStorage = useCloudStorage();
+  const { data: subscription, isLoading } = useQuery({
+    queryKey: ["subscription"],
+    queryFn: async () =>
+      getSubscriptionInfo({
+        token: await cloudStorage.getItem(ACCESS_TOKEN_NAME),
+      }),
+  });
+  const formatDate = (dateString: string): string => {
+    const [year, month, day] = dateString.split("-"); // Разбиваем строку на компоненты
+    return `${day}.${month}.${year}`; // Собираем в нужном формате
   };
+  if (!isLoading || subscription) {
+    return (
+        <section className={cn("wrapper", styles.section)}>
+        <h2 className={styles.section__heading}>Подписка</h2>
+        {!user.subscription ? (
+            <SubscriptionCard
+            title="У тебя пока нет подписки"
+            description="Открывай доступ ко всем заданиям с подпиской!"
+            href={"/"}
+        />
+        ) : (
+            <SubscriptionCard
+            title={`Подписка ${subscription.plan.title}`}
+            description={`Действует до ${formatDate(subscription.end_date)}`}
+            href={"/"}
+        />
+        )}
+        </section>
+        )
+    };
+};
 
 const SubscriptionCard: FC<SubscriptionCardProps> = ({ title, description, isSm = false, href = "" }) => {
 return (
