80be5ed2f476b9f94888e853a3d7e73da4d2ca87
Producer <99910126+SNex21@users.noreply.github.com>
2024-12-28 17:49:21 +0300

    create module of sub types

--- apps/web/src/pages/(authenticated)/subscription/page.tsx
+++ apps/web/src/pages/(authenticated)/subscription/page.tsx
@@ -1,19 +1,11 @@
-import styles from "./Subscribe.module.scss";
-import { useNavigate } from "react-router-dom";
-
 import { useUser } from "@/providers/AuthProvider/AuthProvider";
-import { useCloudStorage } from "@/lib/twa/hooks";
-import { ACCESS_TOKEN_NAME } from "@/services/auth/storage.ts";
-import { useQuery } from "@tanstack/react-query";
-import { getSubscriptionInfo } from "@/services/api/subscriptions";
 import { WithSubscriptionPage } from "./WithSubscription";
 import { NoSubscriptionPage } from "./NoSubscription";
 
 
-
 export default function SubscriptionPage() {
     const user = useUser();
-    
+
     // if (isLoading || !subscription) {
     //     return <SubscriptionSectionLoading />;
     // }
